c9e37750d58d6cf4264c87eb730cef917a00d916
Adrien <a.bonnand@pacman.fr>
2022-05-05 18:45:24 +0200

    tg

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
+<<<<<<< HEAD
 
+=======
+#include "mainwindow.h"
+#include <QApplication>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+>>>>>>> 9ef20324306fc78700d5c9e4b8cf2937ce403169
 
-int main(int argc, char *argv[])
+bool deplacer_personnage(int x, int y, std::string cmd);
+
+int main(int argc, char** argv)
 {
+<<<<<<< HEAD
  return 0;
+=======
+    int x=5, y=4;
+    deplacer_personnage(x,y, "UP");
+
+    return 0;
+}
+
+bool deplacer_personnage(int x, int y, std::string cmd){
+
+>>>>>>> 9ef20324306fc78700d5c9e4b8cf2937ce403169
 }
