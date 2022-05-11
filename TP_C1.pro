0d5fd3d47fc08424e25e527a70c7c5f50384aeef
Adrien <a.bonnand@pacman.fr>
2022-05-11 08:48:28 +0200

    debut colision

--- TP_C1.pro
+++ TP_C1.pro
@@ -9,19 +9,8 @@ CONFIG += c++11
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+    exceptionsizetab.cpp \
    main.cpp
-=======
-    main.cpp
->>>>>>> 9ef20324306fc78700d5c9e4b8cf2937ce403169
-=======
-    main.cpp
->>>>>>> 9ef20324306fc78700d5c9e4b8cf2937ce403169
-=======
-    main.cpp
->>>>>>> 9ef20324306fc78700d5c9e4b8cf2937ce403169
 
 
 
@@ -29,3 +18,6 @@ SOURCES += \
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+HEADERS += \
+    exceptionsizetab.h
--- main.cpp
+++ main.cpp
@@ -6,27 +6,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 9ef20324306fc78700d5c9e4b8cf2937ce403169
-=======
->>>>>>> 9ef20324306fc78700d5c9e4b8cf2937ce403169
-=======
->>>>>>> 9ef20324306fc78700d5c9e4b8cf2937ce403169
+
+
+
+
+
 
 bool deplacer_personnage(int x, int y, std::string cmd);
 
+bool detecter_collision(int ennemis_x[], int ennemis_y[], int nb_ennemis,
+      int x, int y, std::string cmd);
+
 int main(int argc, char** argv)
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
- return 0;
-=======
-=======
->>>>>>> 9ef20324306fc78700d5c9e4b8cf2937ce403169
-=======
->>>>>>> 9ef20324306fc78700d5c9e4b8cf2937ce403169
+int x=5, y=4;
+int enn_x[4] = {1, 2, 3, 4}, enn_y[4] = {1, 2, 3, 4};
+     if (detecter_collision(enn_x, enn_y, 4, x, y))
+     {
+     std::cout << "Collision" << std::endl;
+
+}
+
     int x=5, y=4;
     deplacer_personnage(x,y, "UP");
 
@@ -35,11 +35,11 @@ int main(int argc, char** argv)
 
 bool deplacer_personnage(int x, int y, std::string cmd){
 
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 9ef20324306fc78700d5c9e4b8cf2937ce403169
-=======
->>>>>>> 9ef20324306fc78700d5c9e4b8cf2937ce403169
-=======
->>>>>>> 9ef20324306fc78700d5c9e4b8cf2937ce403169
+
+}
+
+bool detecter_collision(int ennemis_x[], int ennemis_y[], int nb_ennemis,
+      int x, int y, std::string cmd)
+{
+        // contenu
 }
